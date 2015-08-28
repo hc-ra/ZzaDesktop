f77049e0c44a11550a0d7dadcd9dc37f7393d162
Bartosz Migacz <4rchers@gmail.com>
2015-08-28 20:13:31 +0200

    Added scaffold Vs/VMs for customer list, orders, orderprep, customer add/edit. Implemented customer list and navigation.

---
+++ ZzaDesktop/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZzaDesktop.Customers;
+using ZzaDesktop.OrderPrep;
+using ZzaDesktop.Orders;
+using Zza.Data;
+namespace ZzaDesktop
+{
+	class MainWindowViewModel : BindableBase
+	{
+		private CustomerListViewModel _customerListViewModel = new CustomerListViewModel();
+		private OrderViewModel _orderViewModel = new OrderViewModel();
+		private OrderPrepViewModel _orderPrepViewModel = new OrderPrepViewModel();
+		private AddEditCustomerViewModel _addEditViewModel = new AddEditCustomerViewModel();
+
+		private BindableBase _currentViewModel;
+
+		public MainWindowViewModel()
+		{
+			NavCommand = new RelayCommand<string>(OnNav);
+			_customerListViewModel.PlaceOrderRequested += NavToOrder;
+			_customerListViewModel.AddCustomerRequested += NavToAddCustomer;
+			_customerListViewModel.EditCustomerRequested += NavToEditCustomer;
+		}
+
+		private void NavToAddCustomer(Customer customer)
+		{
+			_addEditViewModel.EditMode = true;
+			_addEditViewModel.SetCustomer(customer);
+			CurrentViewModel = _addEditViewModel;
+		}
+
+		private void NavToEditCustomer(Customer customer)
+		{
+			_addEditViewModel.EditMode = false;
+			_addEditViewModel.SetCustomer(customer);
+			CurrentViewModel = _addEditViewModel;
+		}
+
+
+		private void NavToOrder(Guid customerId)
+		{
+			_orderViewModel.CustomerId = customerId;
+			CurrentViewModel = _orderViewModel;
+		}
+		public BindableBase CurrentViewModel
+		{
+			get { return _currentViewModel; }
+			set { SetProperty(ref _currentViewModel, value); }
+		}
+
+		public RelayCommand<string> NavCommand { get; private set; }
+		private void OnNav(string destination)
+		{
+			switch (destination)
+			{
+				case "orderPrep":
+					CurrentViewModel = _orderPrepViewModel;
+					break;
+				case "customers":
+				default:
+					CurrentViewModel = _customerListViewModel;
+					break;
+			}
+		}
+
+	}
+}
